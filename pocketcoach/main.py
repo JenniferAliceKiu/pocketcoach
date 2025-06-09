3ed9e38f4e40470a0bb7fcc3de1aeb7400a5643c
Tobias Urhaug <tobiasurhaug@gmail.com>
2025-06-09 21:36:53 +0200

    Add cleaning method to preprocessing

--- pocketcoach/main.py
+++ pocketcoach/main.py
@@ -1,12 +1,17 @@
-from pocketcoach.ml_logic.data import get_data
+from pocketcoach.ml_logic.data import get_data, clean_data_set
 from pocketcoach.params import *
 from pathlib import Path
 
 def preprocess():
     """
-    Reads the train, test, and validation data sets.
+    - Reads the train, test, and validation data sets.
+    - Performs basic cleaning of the data-sets
     """
 
     train_df = get_data(Path(LOCAL_DATA_PATH).joinpath("training.csv"))
     test_df = get_data(Path(LOCAL_DATA_PATH).joinpath("test.csv"))
     validation_df = get_data(Path(LOCAL_DATA_PATH).joinpath("validation.csv"))
+
+    train_cleaned_df = clean_data_set(train_df)
+    test_cleaned_df = clean_data_set(test_df)
+    validation_cleaned_df = clean_data_set(validation_df)
--- pocketcoach/ml_logic/data.py
+++ pocketcoach/ml_logic/data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from pathlib import Path
+import string
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
 
 def get_data(
         cache_path:Path,
@@ -17,3 +20,43 @@ def get_data(
     print(f"✅ Data loaded from file {cache_path.name}, with shape {df.shape}")
 
     return df
+
+
+def clean_data_set(df: pd.DataFrame):
+    """
+    Adds a column with cleaned texts to the data frame.
+    """
+
+    df['cleaned_text'] = df['text'].apply(clean)
+    print("✅ data cleaned")
+
+    return df
+
+def clean(text: str):
+    """
+    Cleans the raw texts by lower casing, strip for leading or trailing white-
+    spaces, remove digits and punctiations and perform lemmatization on the
+    verbs and nouns.
+    """
+
+    text = text.strip()
+    text = text.lower()
+    text = ''.join(word for word in text if not word.isdigit())
+    for punctuation in string.punctuation:
+        text = text.replace(punctuation, '')
+    tokenized = word_tokenize(text)
+    tokenized_lemmatized = lemmatize(tokenized, "v")
+    tokenized_lemmatized = lemmatize(tokenized_lemmatized, "n")
+    cleaned_text = " ".join(tokenized_lemmatized)
+
+    return cleaned_text
+
+
+def lemmatize(word_tokens, pos):
+    """
+    Lemmatizes word tokens based on pos
+    """
+    return [
+        WordNetLemmatizer().lemmatize(word, pos=pos)
+        for word in word_tokens
+    ]
